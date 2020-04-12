2af4f30418c43488f42e1747c48c213f4176ada2
Lionel Savidan <l.savidan.ubiqum@gmail.com>
2020-04-12 11:54:30 +0200

    display search done

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import SearchForm from './SearchForm';
 import {connect} from 'react-redux';
+
+import SearchForm from './SearchForm';
+import Spinner from '../layout/Spinner';
+import MoviesContainer from './MoviesContainer';
+
 
 
 
 
 class Landing extends Component {
     render() {
+     const {loading} = this.props;
         return (
             <div className= "container">
 
               <SearchForm /> 
-
+{loading ? <Spinner/> : <MoviesContainer/>}
             </div>
         )
     }
